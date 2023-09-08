87a15c606e4343b2f5d4fafe04caa83f1cfe551d
Nixon.trabajo <Nixon.Betancour@co.nestle.com>
2023-09-08 18:20:52 -0500

    update

--- exercies.py
+++ exercies.py
@@ -10,7 +10,10 @@ age = int(input('¿cual es tu edad?:'))
 
 for element in range(age):
     print('has cumpliado ' + str(element+1) + ' años')
-'''
+
 number = int(input('dame un numero entero positivo => '))
 for element in range(1, number+1, 2):
     print(element, end=', ')
+'''
+prueba = True and False
+print(prueba)
