63b40e0c69b407185a9aba8150f2d39084859efa
Nixon.trabajo <Nixon.Betancour@co.nestle.com>
2023-09-21 17:17:10 -0500

    clase comprehension

--- 102_python/05_dict.py
+++ 102_python/05_dict.py
@@ -12,3 +12,16 @@ print(dict)
 dict_v2 = {i : i * 2 for i in range(1, 6)}
 print(dict_v2)
 '''
+# se agrego el valor population a las llaves del diccionario countries con la biblioteca random
+import random
+countries = ['col','pe','mex','bol']
+population = {}
+for country in countries:
+    population[country] = random.randint(1, 100)
+
+print(population)
+
+#usando list comprehension para el ej anterior(usando sintaxis corta)
+
+population_v2 =  {country: random.randint(1, 100) for country in countries}
+print(population_v2)
