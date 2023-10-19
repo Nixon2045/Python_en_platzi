3cbcd7fb530c0c753b6b2503d3b2a374d87539cf
Nixon.trabajo <Nixon.Betancour@co.nestle.com>
2023-10-19 15:18:50 -0500

    clase 12 lambda

---
+++ 102_python/12_lambda.py
@@ -0,0 +1,19 @@
+def increment(x):
+    return x + 1
+result = increment(10)
+print(result)
+
+#en la funciona lambda la sintaxis es mas corta y los parametros de entrada van primero seguidos 
+# del cuerpo o retorno de la funcion como vemos en el siguiente ejemplo 
+
+increment_v2 = lambda x : x + 1 
+
+result = increment_v2(20)
+print(result)
+
+
+# ejemplo de otro lambda
+
+caso_x = lambda name, last_name: f'the fullname is {name.title()} {last_name.title()}'
+result = caso_x('nixon', 'betancour')
+print(result) 
